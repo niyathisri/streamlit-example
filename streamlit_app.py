3e79b7e22c2219cfedc99bd2a8b31ce36f52a02e
Niyathi Srinivasan <109157896+niyathisri@users.noreply.github.com>
2022-10-16 19:13:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,166 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import numpy as np
+import pandas as pd
+import keras
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential, load_model
+from keras import backend as K
+import os
+import time
+import io
+from PIL import Image
+import plotly.express as px
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+MODELSPATH = '/work/goodmultimodel.h5' #change
+DATAPATH = '/work/MonkeypoxMulticlass'  #change
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def render_header():
+    st.write("""
+        <p align="center"> 
+            <H1> Skin Disease Classifier 
+        </p>
+    """, unsafe_allow_html=True)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+@st.cache
+def load_mekd():
+    img = Image.open(DATAPATH) #change
+    return img
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+@st.cache
+def data_gen(x):
+    img = np.asarray(Image.open(x).resize((100, 75)))
+    x_test = np.asarray(img.tolist())
+    x_test_mean = np.mean(x_test)
+    x_test_std = np.std(x_test)
+    x_test = (x_test - x_test_mean) / x_test_std
+    x_validate = x_test.reshape(1, 75, 100, 3)
+
+    return x_validate
+
+
+@st.cache
+def data_gen_(img):
+    img = img.reshape(100, 75)
+    x_test = np.asarray(img.tolist())
+    x_test_mean = np.mean(x_test)
+    x_test_std = np.std(x_test)
+    x_test = (x_test - x_test_mean) / x_test_std
+    x_validate = x_test.reshape(1, 75, 100, 3)
+
+    return x_validate
+
+
+def load_models():
+
+    model = load_model('goodmultimodel.h5')
+    return model
+
+
+@st.cache
+def predict(x_test, model):
+    Y_pred = model.predict(x_test)
+    ynew = model.predict_proba(x_test)
+    K.clear_session()
+    ynew = np.round(ynew, 2)
+    ynew = ynew*100
+    y_new = ynew[0].tolist()
+    Y_pred_classes = np.argmax(Y_pred, axis=1)
+    K.clear_session()
+    return y_new, Y_pred_classes
+
+
+@st.cache
+def display_prediction(y_new):
+    """Display image and preditions from model"""
+
+    result = pd.DataFrame({'Probability': y_new}, index=np.arange(7))
+    result = result.reset_index()
+    result.columns = ['Classes', 'Probability']
+    # change lesion_type_dict = {2: 'Benign keratosis-like lesions', 4: 'Melanocytic nevi', 3: 'Dermatofibroma',
+                        5: 'Melanoma', 6: 'Vascular lesions', 1: 'Basal cell carcinoma', 0: 'Actinic keratoses'}
+    result["Classes"] = result["Classes"].map(lesion_type_dict)
+    return result
+
+
+def main():
+    st.sidebar.header('Skin Disease Analyzer')
+    st.sidebar.subheader('Choose a page to proceed:')
+    page = st.sidebar.selectbox("", ["Sample Data", "Upload Your Image"])
+
+    if page == "Sample Data":
+        st.header("Sample Data Prediction for Skin Disease")
+        st.markdown("""
+        **Now, this is probably why you came here. Let's get you some Predictions**
+        You need to choose Sample Data
+        """)
+
+        mov_base = ['Sample Data I']
+        movies_chosen = st.multiselect('Choose Sample Data', mov_base)
+
+        if len(movies_chosen) > 1:
+            st.error('Please select Sample Data')
+        if len(movies_chosen) == 1:
+            st.success("You have selected Sample Data")
+        else:
+            st.info('Please select Sample Data')
+
+        if len(movies_chosen) == 1:
+            if st.checkbox('Show Sample Data'):
+                st.info("Showing Sample data---->>>")
+                image = load_mekd()
+                st.image(image, caption='Sample Data', use_column_width=True)
+                st.subheader("Choose Training Algorithm!")
+                if st.checkbox('Keras'):
+                    model = load_models()
+                    st.success("Hooray !! Keras Model Loaded!")
+                    if st.checkbox('Show Prediction Probablity on Sample Data'):
+                        x_test = data_gen(DATAPATH + '/ISIC_0024312.jpg')
+                        y_new, Y_pred_classes = predict(x_test, model)
+                        result = display_prediction(y_new)
+                        st.write(result)
+                        if st.checkbox('Display Probability Graph'):
+                            fig = px.bar(result, x="Classes",
+                                         y="Probability", color='Classes')
+                            st.plotly_chart(fig, use_container_width=True)
+
+# from collections import namedtuple
+# import altair as alt
+# import math
+# import pandas as pd
+# import streamlit as st
+
+# """
+# # Welcome to Streamlit!
+
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
+
+# In the meantime, below is an example of what you can do with just a few lines of code:
+# """
+
+
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+#     Point = namedtuple('Point', 'x y')
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
